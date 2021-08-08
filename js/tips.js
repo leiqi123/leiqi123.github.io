17c0348ec061cfddccb93d3f2104d785fb478345
leiqi123 <88603280+leiqi123@users.noreply.github.com>
2021-08-08 10:42:52 +0800

    first add files

---
+++ js/tips.js
@@ -0,0 +1,3 @@
+	function openwin(){	
+			window.open ("tips.html", "newwindow", "height=100, width=400, top=300, left=550, toolbar=no, menubar=no, scrollbars=no, resizable=no,location=no, status=no")
+}
